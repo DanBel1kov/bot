bc60c2f9bee022be546819a5cf38162e82f2e265
Mikzarjr <107720964+Mikzarjr@users.noreply.github.com>
2023-04-18 14:50:06 +0300

    1

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print('hi bitch')
+from aiogram import Bot, Dispatcher, types, executor
+
+TOKEN_API = "6156845668:AAG7Esqiw1MT9VBX0qZRnfMEmtCDYag3i-U"
+
+bot = Bot(TOKEN_API)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler()
+async def echo_upper(message: types.Message):
+    await message.answer(message.text)
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp)
