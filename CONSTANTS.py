76c33e4ef8b7454dc4c7620aabbd8c2a4314c5b4
dan4ous7562 <114076646+dan4ous7562@users.noreply.github.com>
2023-04-25 18:35:45 +0300

    Update CONSTANTS.py

--- CONSTANTS.py
+++ CONSTANTS.py
@@ -1,13 +1,56 @@
-TOKEN_API = "6156845668:AAG7Esqiw1MT9VBX0qZRnfMEmtCDYag3i-U"
+
+
+API_TOKEN = "5942173865:AAF6goSFqJACn02FGPK9gMy_J8-tAFjrrHs"
+CHAT_ID = "@syetnoibot"
+CHANNEL_ID = -1001738772333
+CHANNEL_LINK = 'https://t.me/ggrtcvnjj'
 
 HELP_COMMAND = """
-/help 
-/start
-                """
+/start - перезапуск бота
+/help - список команд
+/register - пройти регистрацию 
+               """
 
 
 def START_MESSAGE(name):
-    return "Привет, {}! {}".format(name, privetstvenniy_text)
+    return "Привет, {}! {}".format(name, INTRO_TEXT)
 
 
-privetstvenniy_text = "Ла ла ла лень писать"  # ВАШ ПРИВЕТСТВЕННЫЙ ТЕКСТ
+INTRO_TEXT = """
+Крутое вступительное сообщение которое мне лень придумывать! 
+Для полного списка команд напиши мне /help
+             """
+
+NOT_SUBSCRIBED_MESSAGE = """
+Для доступа к функционалу бота подпишитесь на группу
+                         """
+NOT_SUBSCRIBED_MESSAGE2 = """
+Для продолжения вы должны подписаться на группу
+                         """
+
+BANNED_TEXT = """
+К сожалению, вы не соблюдали правила сообщества и поэтому доступ к боту вам запрещен.
+              """
+
+REGISTRATION_MESSAGE_1 = """
+                         """
+REGISTRATION_MESSAGE_2 = """
+Для регистрации расскажи мне немного о себе:
+
+Как тебя зовут?
+(ФИО как в паспорте)
+                         """
+REGISTRATION_MESSAGE_3 = """
+Когда у тебя день рождения
+(формат: YYYY-MM-DD)
+                         """
+REGISTRATION_MESSAGE_4 = """
+Ваш номер телефона
+(формат: +70007777777)
+                         """
+REGISTRATION_MESSAGE_5 = """
+Твой пол
+(М/Ж)
+                         """
+
+CONFIRM_REGISTRATION_MSG = ("Спасибо, теперь вы есть в базе!")
